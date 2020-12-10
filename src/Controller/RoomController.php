5a4550d80491576e34e55081f2d3bef9b5dbd6c0
Mateusz Piechnat <mateusz@piechnat.pl>
2020-12-10 15:09:13 +0100

    Redirect to room_show after room add

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -61,13 +61,14 @@ class RoomController extends AbstractController
         $form = $this->createForm(RoomType::class);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+            $room = $form->getData();
             $em = $this->getDoctrine()->getManager();
-            $em->persist($form->getData());
+            $em->persist($room);
             $em->flush();
 
-            return $this->redirectToRoute('room_index');
+            return $this->redirectToRoute('room_show', ['id' => $room->getId()]);
         }
-
+        
         return $this->render('room/add.html.twig', ['form' => $form->createView()]);
     }
 
